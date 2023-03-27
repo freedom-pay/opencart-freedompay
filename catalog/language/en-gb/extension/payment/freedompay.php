1e7d083efe76d8991bff43c0146ffa27e3e22e53
freedompay <cms@freedompay.money>
2023-03-27 08:53:46 +0000

    3.0.3

---
+++ catalog/language/en-gb/extension/payment/freedompay.php
@@ -0,0 +1,7 @@
+<?php
+
+$_['text_title'] = 'Freedom Pay (Visa, Mastercard)';
+$_['msg_description'] = 'Оплата по счету №%s';
+$_['err_order_not_found'] = 'Заказ не найден! Повторите попытку или свяжитесь с администрацией!';
+$_['button_confirm'] = 'Подтвердить заказ';
+$_['button_back'] = 'Вернуться';
